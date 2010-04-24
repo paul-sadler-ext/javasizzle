9c79f4acd7fdabeb0aa65d891452491a15cd277e
george@svdriving.co.uk <george@svdriving.co.uk@85f30d1c-feab-11de-9adb-2bc960bfb704>
2010-04-24 06:59:04 +0000

    Major refactor to make invariables not extend Predicate<Void>. New utility class Invariables required to replace lost Predicates functionality.

--- trunk/JSizzle/src/org/jsizzle/Invariable.java
+++ trunk/JSizzle/src/org/jsizzle/Invariable.java
@@ -5,17 +5,31 @@ import java.util.Map.Entry;
 
 import com.google.common.base.Predicate;
 
-public interface Invariable extends Predicate<Void>
+public interface Invariable
 {
+    /**
+     * @return All violations of this invariable and its contained invariables.
+     *         Note that the returned iterable may not be empty even if this
+     *         invariable does not violate its invariant, because its contained
+     *         invariables may be disjoint.
+     */
     @AsFunction
     Iterable<? extends Entry<? extends Invariable, Set<String>>> getViolations();
+
+    /**
+     * @return whether this invariable violates its invariant.
+     *         Note this method is not made available as a function because a
+     *         predicate is more useful.
+     * @see #invariant
+     */
+    boolean invariant();
     
-    final Predicate<Invariable> application = new Predicate<Invariable>()
+    static final Predicate<Invariable> invariant = new Predicate<Invariable>()
     {
         @Override
-        public boolean apply(Invariable input)
+        public boolean apply(Invariable invariable)
         {
-            return input.apply(null);
+            return invariable.invariant();
         }
     };
 }
