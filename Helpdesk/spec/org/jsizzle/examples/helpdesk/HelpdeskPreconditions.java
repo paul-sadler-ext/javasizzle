4ed5dd60b44fc6b15a10462799a22fd1388c7032
george <george@svdriving.co.uk>
2010-04-25 14:40:08 +0000

    Added exploratory precondition test and Tuple and Type utilities

---
+++ Helpdesk/spec/org/jsizzle/examples/helpdesk/HelpdeskPreconditions.java
@@ -0,0 +1,27 @@
+package org.jsizzle.examples.helpdesk;
+
+import java.util.Collections;
+
+import org.jsizzle.Prime;
+import org.jsizzle.examples.helpdesk.HelpdeskSpec.Analyst;
+import org.jsizzle.examples.helpdesk.HelpdeskSpec.Customer;
+import org.jsizzle.examples.helpdesk.HelpdeskSpec.Id;
+import org.jsizzle.examples.helpdesk.HelpdeskSpec.Issue;
+import org.jsizzle.examples.helpdesk.HelpdeskSpec.Note;
+import org.jsizzle.examples.helpdesk.HelpdeskSpec.Status;
+import org.junit.Test;
+
+public class HelpdeskPreconditions
+{
+    @Test
+    public void preIssueInit()
+    {
+        new Issue.Init(new Prime<Issue>(new Issue(new Customer("Fred"),
+                                                  new Analyst("Barney"),
+                                                  Collections.<Id> emptySet(),
+                                                  Collections.<Note> emptyList(),
+                                                  Status.OPEN)),
+                       new Customer("Fred"),
+                       new Analyst("Barney")).checkInvariant();
+    }
+}
