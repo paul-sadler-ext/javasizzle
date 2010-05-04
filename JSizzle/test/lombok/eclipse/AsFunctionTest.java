06d7d66e2d5e8dad6e5d044dd644366c64d716c8
george <george@svdriving.co.uk>
2010-05-04 17:27:15 +0000

    Setting up unit test for AsFunction currying

--- JSizzle/test/lombok/eclipse/AsFunctionTest.java
+++ JSizzle/test/lombok/eclipse/AsFunctionTest.java
@@ -6,6 +6,8 @@ import lombok.core.PrintAST;
 import org.jsizzle.AsFunction;
 import org.junit.Test;
 
+import com.google.common.base.Function;
+
 @PrintAST
 public class AsFunctionTest
 {
@@ -26,7 +28,7 @@ public class AsFunctionTest
     {
         return 1;
     }
-    
+
     private static class OneArgConstructor
     {
         public int x;
@@ -60,5 +62,61 @@ public class AsFunctionTest
     public void testOneArgConstructor()
     {
         assertEquals(1, OneArgConstructor.oneArgConstructor.apply(1).x);
+    }
+    
+    @Test
+    public void testThreeArgStatic()
+    {
+        assertEquals(6, add.apply(1).apply(2).apply(3).intValue());
+    }
+
+    static Function<Integer, Function<Integer, Function<Integer, Integer>>> add = new add();
+
+    static final class add implements Function<Integer, Function<Integer, Function<Integer, Integer>>>
+    {
+        @Override
+        public Function<Integer, Function<Integer, Integer>> apply(final Integer x)
+        {
+            return new $addY(x);
+        }
+    }
+
+    static final class $addY implements Function<Integer, Function<Integer, Integer>>
+    {
+        private final Integer x;
+
+        private $addY(Integer x)
+        {
+            this.x = x;
+        }
+
+        @Override
+        public Function<Integer, Integer> apply(final Integer y)
+        {
+            return new $addZ(x, y);
+        }
+    }
+
+    static final class $addZ implements Function<Integer, Integer>
+    {
+        private final Integer x;
+        private final Integer y;
+
+        private $addZ(Integer x, Integer y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public Integer apply(final Integer z)
+        {
+            return add(x, y, z);
+        }
+    }
+
+    static int add(int x, int y, int z)
+    {
+        return x + y + z;
     }
 }
