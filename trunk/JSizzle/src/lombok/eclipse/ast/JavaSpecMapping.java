09585b5d6974a4d7c3dbc33713bb54b3ede7af91
george@svdriving.co.uk <george@svdriving.co.uk@85f30d1c-feab-11de-9adb-2bc960bfb704>
2010-06-06 16:23:53 +0000

    Created spec mapping for making a schema

--- trunk/JSizzle/src/lombok/eclipse/ast/JavaSpecMapping.java
+++ trunk/JSizzle/src/lombok/eclipse/ast/JavaSpecMapping.java
@@ -3,16 +3,17 @@ package lombok.eclipse.ast;
 import static com.google.common.base.Functions.compose;
 import static com.google.common.collect.Iterables.filter;
 import static com.google.common.collect.Iterables.transform;
+import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Lists.transform;
 import static com.google.common.collect.Maps.newHashMap;
 import static com.google.common.collect.Sets.newHashSet;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.emptySet;
+import static lombok.eclipse.Eclipse.fromQualifiedName;
 import static lombok.eclipse.Eclipse.toQualifiedName;
 import static lombok.eclipse.ast.AstUtilities.findLocalType;
 import static lombok.eclipse.ast.AstUtilities.fullyQualifiedName;
-import static org.eclipse.jdt.internal.compiler.ast.ASTNode.IsMemberType;
 import static org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants.AccAnnotation;
 import static org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants.AccEnum;
 import static org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants.AccFinal;
@@ -21,7 +22,6 @@ import static org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants.AccP
 import static org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants.AccProtected;
 import static org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants.AccPublic;
 import static org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants.AccStatic;
-import static org.jcurry.ValueObjects.toSet;
 
 import java.util.List;
 import java.util.Map;
@@ -43,6 +43,7 @@ import org.jsizzle.JavaSpec.MetaType;
 import org.jsizzle.JavaSpec.Method;
 import org.jsizzle.JavaSpec.Modifier;
 import org.jsizzle.JavaSpec.Name;
+import org.jsizzle.JavaSpec.PrimitiveName;
 import org.jsizzle.JavaSpec.Type;
 import org.jsizzle.JavaSpec.TypeName;
 import org.jsizzle.JavaSpec.TypeScope;
@@ -51,9 +52,11 @@ import org.jsizzle.JavaSpec.Visibility;
 import org.jsizzle.SchemaSpec.JSizzleTypeName;
 
 import com.google.common.base.Function;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import com.sun.xml.internal.org.jvnet.staxex.NamespaceContextEx.Binding;
 
-public abstract class JavaSpecMapping
+public class JavaSpecMapping
 {
     private static final Map<String, TypeName> fixedTypeNames = newHashMap();
     static
@@ -64,13 +67,32 @@ public abstract class JavaSpecMapping
         fixedTypeNames.put(Object.class.getName(), JavaLangTypeName.OBJECT);
         fixedTypeNames.put(Enum.class.getName(), JavaLangTypeName.ENUM);
         fixedTypeNames.put(null, JavaLangTypeName.NONE);
+        fixedTypeNames.put("byte", PrimitiveName.BYTE);
+        fixedTypeNames.put("short", PrimitiveName.SHORT);
+        fixedTypeNames.put("int", PrimitiveName.INT);
+        fixedTypeNames.put("long", PrimitiveName.LONG);
+        fixedTypeNames.put("char", PrimitiveName.CHAR);
+        fixedTypeNames.put("boolean", PrimitiveName.BOOLEAN);
+        fixedTypeNames.put("double", PrimitiveName.DOUBLE);
+        fixedTypeNames.put("float", PrimitiveName.FLOAT);
     }
-    private final Map<String, TypeName> typeNames = newHashMap(fixedTypeNames);
+    private final BiMap<String, TypeName> typeNames = HashBiMap.create(fixedTypeNames);
     private final CompilationUnitDeclaration compilationUnit;
     
     public JavaSpecMapping(CompilationUnitDeclaration compilationUnit)
     {
         this.compilationUnit = compilationUnit;
+    }
+    
+    @AsFunction
+    public Type typeForName(TypeName typeName)
+    {
+        final String qualifiedName = typeNames.inverse().get(typeName);
+        final String packageName = toQualifiedName(compilationUnit.currentPackage.tokens);
+        if (qualifiedName != null && qualifiedName.startsWith(packageName))
+            return specType(compilationUnit.declarationOfType(fromQualifiedName(qualifiedName.substring(packageName.length() + 1))));
+        
+        return null;
     }
 
     @AsFunction
@@ -79,14 +101,14 @@ public abstract class JavaSpecMapping
         return new Type(specVisibility(type.modifiers),
                         specOtherModifiers(type.modifiers),
                         specSet(type.annotations, specAnnotation.apply(type)),
-                        specTypeName(fullyQualifiedName(type, compilationUnit)),
+                        specClassName(fullyQualifiedName(type, compilationUnit)),
                         specMetaType(type.modifiers),
-                        specTypeScope(type.bits),
+                        type.enclosingType != null ? TypeScope.MEMBER : TypeScope.TOP,
                         specSet(type.fields, specVariable.apply(type)),
                         specSet(type.methods, specConstructor.apply(type), ConstructorDeclaration.class),
                         specSet(type.methods, specMethod.apply(type), MethodDeclaration.class),
                         specSet(type.memberTypes, specType),
-                        specTypeName(type.enclosingType, type.superclass.getTypeName()),
+                        specReference(type.enclosingType, type.superclass),
                         specSet(type.superInterfaces, specReference.apply(type.enclosingType)));
     }
     
@@ -99,7 +121,7 @@ public abstract class JavaSpecMapping
                           specList(method.arguments, specVariable.apply(scope)),
                           new Name(new String(method.selector)),
                           specList(method.arguments, compose(Variable.getTypeName, specVariable.apply(scope))),
-                          specTypeName(scope, method.returnType.getTypeName()));
+                          specReference(scope, method.returnType));
     }
     
     @AsFunction
@@ -118,30 +140,25 @@ public abstract class JavaSpecMapping
                             specOtherModifiers(variable.modifiers),
                             specSet(variable.annotations, specAnnotation.apply(scope)),
                             new Name(new String(variable.name)),
-                            specTypeName(scope, variable.type.getTypeName()));
+                            specReference(scope, variable.type));
     }
 
-    public static TypeScope specTypeScope(int bits)
-    {
-        return (bits & IsMemberType) != 0 ? TypeScope.MEMBER : TypeScope.TOP;
-    }
-    
     public <A, S> Set<S> specSet(A[] astns, Function<A, S> specA)
     {
         final Set<S> noSpecs = emptySet();
-        return astns == null ? noSpecs : toSet(transform(asList(astns), specA));
+        return astns == null ? noSpecs : newHashSet(transform(asList(astns), specA));
     }
     
     public <A, D extends A, S> Set<S> specSet(A[] astns, Function<D, S> specA, Class<D> typeD)
     {
         final Set<S> noSpecs = emptySet();
-        return astns == null ? noSpecs : toSet(transform(filter(asList(astns), typeD), specA));
+        return astns == null ? noSpecs : newHashSet(transform(filter(asList(astns), typeD), specA));
     }
     
     public <A, S> List<S> specList(A[] astns, Function<A, S> specA)
     {
         final List<S> noSpecs = emptyList();
-        return astns == null ? noSpecs : transform(asList(astns), specA);
+        return astns == null ? noSpecs : newArrayList(transform(asList(astns), specA));
     }
     
     @AsFunction
@@ -153,20 +170,21 @@ public abstract class JavaSpecMapping
     @AsFunction
     public TypeName specReference(TypeDeclaration scope, TypeReference implemented)
     {
-        return specTypeName(scope, implemented.getTypeName());
+        return specTypeName(scope, implemented == null ? null : implemented.getTypeName());
     }
     
     public TypeName specTypeName(TypeDeclaration scope, final char[][] localName)
     {
-        final TypeDeclaration localType = findLocalType(compilationUnit, scope, localName);
-        return specTypeName(localType == null ? localName : fullyQualifiedName(localType, compilationUnit));
+        final TypeDeclaration localType = localName == null ? null : findLocalType(compilationUnit, scope, localName);
+        return specClassName(localType == null ? localName : fullyQualifiedName(localType, compilationUnit));
     }
     
-    public TypeName specTypeName(final char[][] qualifiedName)
+    public TypeName specClassName(final char[][] qualifiedName)
     {
-        TypeName typeName = typeNames.get(toQualifiedName(qualifiedName));
+        final String key = qualifiedName == null ? null : toQualifiedName(qualifiedName);
+        TypeName typeName = typeNames.get(key);
         if (typeName == null)
-            typeNames.put(toQualifiedName(qualifiedName), typeName = new TypeName() {});
+            typeNames.put(key, typeName = new TypeName() {});
         return typeName;
     }
 
@@ -201,6 +219,6 @@ public abstract class JavaSpecMapping
         else if ((modifiers & AccPrivate) != 0)
             return Visibility.PRIVATE;
         else
-            return Visibility.PUBLIC;
+            return Visibility.DEFAULT;
     }
 }
