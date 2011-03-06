f89dc88875c66d5e1edb1a3241c7f1d3a688e25e
GSvarovsky <GSvarovsky@85f30d1c-feab-11de-9adb-2bc960bfb704>
2011-03-06 10:07:02 +0000

    Formatting changes for write-up

--- trunk/Helpdesk/test/org/jsizzle/examples/helpdesk/impl/HelpdeskImplTest.java
+++ trunk/Helpdesk/test/org/jsizzle/examples/helpdesk/impl/HelpdeskImplTest.java
@@ -7,38 +7,34 @@ import org.junit.Test;
 
 public class HelpdeskImplTest
 {
-    private final HelpdeskImpl.Instrumented helpdesk = new HelpdeskImpl().new Instrumented(); 
-    
-    /**
-     * When a customer calls in to the support hotline, an analyst will create
-     * an open issue in the system for that customer. The issue is initially
-     * assigned to the analyst that created it.
-     */
-    @Test
-    public void addIssue()
+    private final HelpdeskImpl.Instrumented helpdesk =
+        new HelpdeskImpl().new Instrumented();
+
+    /* When a customer calls in to the support hotline, an analyst
+     * will create an open issue in the system for that customer. The
+     * issue is initially assigned to the analyst that created it. */
+    @Test public void addIssue()
     {
-        helpdesk.addIssue(helpdesk.addCustomer("Fred"), helpdesk.addAnalyst("Barney"));
+        helpdesk.addIssue(helpdesk.addCustomer("Fred"),
+                          helpdesk.addAnalyst("Barney"));
     }
 
-    /**
-     * It must be possible to assign the issue to another analyst.
-     */
-    @Test
-    public void reassignIssue()
+    /* It must be possible to assign the issue to another analyst. */
+    @Test public void reassignIssue()
     {
-        final Issue issue = helpdesk.addIssue(helpdesk.addCustomer("Fred"), helpdesk.addAnalyst("Barney"));
+        final Issue issue = helpdesk.addIssue(
+                                helpdesk.addCustomer("Fred"),
+                                helpdesk.addAnalyst("Barney"));
         issue.setAnalyst(helpdesk.addAnalyst("Wilma"));
     }
 
-    /**
-     * A number of different notes can be added to the issue as the analyst
-     * works through it.
-     */
-    @Test
-    public void addDifferentNotes()
+    /* A number of different notes can be added to the issue as the
+     * analyst works through it. */
+    @Test public void addDifferentNotes()
     {
         final Person barney = helpdesk.addAnalyst("Barney");
-        final Issue issue = helpdesk.addIssue(helpdesk.addCustomer("Fred"), barney);
+        final Issue issue = helpdesk.addIssue(
+                                helpdesk.addCustomer("Fred"), barney);
         issue.addNote(barney, "Note1");
         issue.addNote(barney, "Note2");
         issue.addNote(barney, "Note3");
@@ -47,16 +43,15 @@ public class HelpdeskImplTest
     @Test(expected = IllegalArgumentException.class)
     public void cannotAddNoteFromWrongAnalyst()
     {
-        final Issue issue = helpdesk.addIssue(helpdesk.addCustomer("Fred"), helpdesk.addAnalyst("Barney"));
+        final Issue issue = helpdesk.addIssue(
+                                helpdesk.addCustomer("Fred"),
+                                helpdesk.addAnalyst("Barney"));
         issue.addNote(helpdesk.addAnalyst("Wilma"), "Note1");
     }
 
-    /**
-     * An issue can be cross-referenced to a number of other issues if the
-     * analyst finds out that they are related.
-     */
-    @Test
-    public void crossReferenceIssue()
+    /* An issue can be cross-referenced to a number of other issues if the
+     * analyst finds out that they are related. */
+    @Test public void crossReferenceIssue()
     {
         final Person fred = helpdesk.addCustomer("Fred");
         final Person barney = helpdesk.addAnalyst("Barney");
@@ -67,17 +62,12 @@ public class HelpdeskImplTest
         issue1.addReference(issue3);
     }
 
-    /**
-     * There are several types of note.
-     * <ul>
-     * <li>A text note allows the analyst to enter free text only.</li>
-     * <li>An attachment note allows the analyst to attach any file.</li>
-     * <li>A resolution note allows the analyst to record a reason of “user
-     * error”, “bug” or “enhancement” against the issue.</li>
-     * </ul>
-     */
-    @Test
-    public void addAllNoteTypes()
+    /* There are several types of note.
+     * - A text note allows the analyst to enter free text only.
+     * - An attachment note allows the analyst to attach any file.
+     * - A resolution note allows the analyst to record a reason of “user
+     * error”, “bug” or “enhancement” against the issue. */
+    @Test public void addAllNoteTypes()
     {
         final Person barney = helpdesk.addAnalyst("Barney");
         final Issue issue = helpdesk.addIssue(helpdesk.addCustomer("Fred"), barney);
@@ -86,11 +76,8 @@ public class HelpdeskImplTest
         issue.addNote(barney, Issue.Resolution.BUG);
     }
     
-    /**
-     * A resolution note... should be the last note added to the issue.
-     */
-    @Test(expected = IllegalStateException.class)
-    public void cannotAddNoteAfterResolution()
+    /* A resolution note... should be the last note added to the issue. */
+    @Test(expected = IllegalStateException.class) public void cannotAddNoteAfterResolution()
     {
         final Person barney = helpdesk.addAnalyst("Barney");
         final Issue issue = helpdesk.addIssue(helpdesk.addCustomer("Fred"), barney);
@@ -98,11 +85,8 @@ public class HelpdeskImplTest
         issue.addNote(barney, "Note1");
     }
     
-    /**
-     * Once an issue is resolved, it can be closed.
-     */
-    @Test
-    public void closeResolvedIssue()
+    /* Once an issue is resolved, it can be closed. */
+    @Test public void closeResolvedIssue()
     {
         final Person barney = helpdesk.addAnalyst("Barney");
         final Issue issue = helpdesk.addIssue(helpdesk.addCustomer("Fred"), barney);
@@ -110,19 +94,15 @@ public class HelpdeskImplTest
         issue.close();
     }
 
-    @Test(expected = IllegalStateException.class)
-    public void cannotCloseUnresolvedIssue()
+    @Test(expected = IllegalStateException.class) public void cannotCloseUnresolvedIssue()
     {
         final Person barney = helpdesk.addAnalyst("Barney");
         final Issue issue = helpdesk.addIssue(helpdesk.addCustomer("Fred"), barney);
         issue.close();
     }
     
-    /**
-     * A list of the issues that an analyst currently has open.
-     */
-    @Test
-    public void reportOpenAnalystIssues()
+    /* A list of the issues that an analyst currently has open. */
+    @Test public void reportOpenAnalystIssues()
     {
         final Person barney = helpdesk.addAnalyst("Barney");
         final Person fred = helpdesk.addCustomer("Fred");
