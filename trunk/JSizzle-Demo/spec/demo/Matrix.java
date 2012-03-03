b391fd98b1157edee9ea35aa2146c85127c9da88
GSvarovsky <GSvarovsky@85f30d1c-feab-11de-9adb-2bc960bfb704>
2012-03-03 14:59:24 +0000

    Tidy up & reset for presentation

--- trunk/JSizzle-Demo/spec/demo/Matrix.java
+++ trunk/JSizzle-Demo/spec/demo/Matrix.java
@@ -1,54 +1,6 @@
 package demo;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-import org.jsizzle.Invariant;
-import org.jsizzle.Schema;
-
-@Schema class Matrix
+class Matrix
 {
-    class Value {}
-    class Name {}
-    
-    class Category
-    {
-        Name name;
-        List<Name> items;
-        
-        @Invariant boolean atLeastOneItem()
-        {
-            return !items.isEmpty();
-        }
-    }
-    
-    Set<Category> categories;
-    
-    class Address
-    {
-        Map<Category, Name> categoryItems;
-        
-        @Invariant boolean itemsValid()
-        {
-            for (Category c : categoryItems.keySet())
-            {
-                if (!c.items.contains(categoryItems.get(c)))
-                    return false;
-            }
-            return true;
-        }
-    }
-    
-    Map<Address, Value> cells;
-    
-    @Invariant boolean addressesOk()
-    {
-        for (Address a : cells.keySet())
-        {
-            if (!a.categoryItems.keySet().equals(categories))
-                return false;
-        }
-        return true;
-    }
 }
--- trunk/JSizzle-Demo/test/demo/AnimateMatrix.java
+++ trunk/JSizzle-Demo/test/demo/AnimateMatrix.java
@@ -1,23 +1,10 @@
 package demo;
 
-import static java.util.Collections.EMPTY_MAP;
-import static java.util.Collections.singleton;
-import static java.util.Collections.singletonList;
-
-import java.util.Set;
-
 import org.junit.Test;
-
-import demo.Matrix.Category;
 
 public class AnimateMatrix
 {
-    @SuppressWarnings("unchecked") @Test public void createMatrix()
+    @Test public void createMatrix()
     {
-        Set<Category> categories =
-            singleton(new Matrix.Category(new Matrix.Name("Fred"),
-                                          singletonList(new Matrix.Name("Fred1"))));
-        Matrix matrix = new Matrix(categories, EMPTY_MAP);
-        matrix.checkInvariant();
     }
 }
