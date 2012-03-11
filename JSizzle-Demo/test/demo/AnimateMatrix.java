96686ba14c493fe2630fdcc9e95d764cb9f9d157
GSvarovsky <GSvarovsky@85f30d1c-feab-11de-9adb-2bc960bfb704>
2012-03-11 12:09:02 +0000

    State after demo

--- JSizzle-Demo/test/demo/AnimateMatrix.java
+++ JSizzle-Demo/test/demo/AnimateMatrix.java
@@ -1,10 +1,17 @@
 package demo;
 
+import static java.util.Collections.EMPTY_MAP;
+import static java.util.Collections.EMPTY_SET;
+
 import org.junit.Test;
+
 
 public class AnimateMatrix
 {
-    @Test public void createMatrix()
+    @Test public void animate()
     {
+        @SuppressWarnings("unchecked")
+        Matrix m = new Matrix(EMPTY_SET, EMPTY_MAP);
+        m.checkInvariant();
     }
 }
