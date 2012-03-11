250a9f0f5e69f40bf79ebd043f33a9517a30d6a5
GSvarovsky <GSvarovsky@85f30d1c-feab-11de-9adb-2bc960bfb704>
2012-03-11 12:09:02 +0000

    State after demo

--- trunk/JSizzle-Demo/spec/demo/Matrix.java
+++ trunk/JSizzle-Demo/spec/demo/Matrix.java
@@ -1,6 +1,64 @@
 package demo;
 
+import static com.google.common.base.Predicates.compose;
+import static com.google.common.base.Predicates.equalTo;
+import static com.google.common.collect.Iterables.all;
 
-class Matrix
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.jsizzle.Invariant;
+import org.jsizzle.Schema;
+
+
+@Schema class Matrix
 {
+    class Name {}
+    class Value {}
+    
+    class Category
+    {
+        Name name;
+        List<Name> items;
+        
+        @Invariant boolean ok()
+        {
+            return !items.isEmpty();
+        }
+    }
+    
+    class Address
+    {
+        Map<Category, Name> categoryItems;
+        
+        Set<Category> categories()
+        {
+            return categoryItems.keySet();
+        }
+        
+        @Invariant boolean ok()
+        {
+            for (Category c : categoryItems.keySet())
+            {
+                if (!c.items.contains(categoryItems.get(c)))
+                    return false;
+            }
+            return true;
+        }
+    }
+    
+    Set<Category> categories;
+    Map<Address, Value> cells;
+    
+    @Invariant boolean ok()
+    {
+        // All addresses must include all categories from the matrix
+        return all(cells.keySet(), compose(equalTo(categories), Address.categories));
+    }
+    
+    @Invariant boolean atLeastOneCategory()
+    {
+        return !categories.isEmpty();
+    }
 }
